018a752493cfb7696fd97545d7d8158a0fd98672
Mirsad Makalic <mirsadm@gmail.com>
2021-10-22 21:38:26 +0100

    - Build system fixes

--- setupenv.sh
+++ setupenv.sh
@@ -270,7 +270,7 @@ popd # tmp
 
 # Compile utilities
 if [[ "$OSTYPE" == "darwin"* ]]; then
-	export CPLUS_INCLUDE_PATH=/usr/local/include:/usr/local/include/opencv4
+	export CPLUS_INCLUDE_PATH=/usr/local/include:/usr/local/include/opencv4	
 elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
 	export CPLUS_INCLUDE_PATH=/usr/include:/usr/include/opencv4
 fi
