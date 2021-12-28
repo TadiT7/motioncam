e79e9b44e148bd4588ddf4ceb45f16647957c721
Mirsad Makalic <mirsadm@gmail.com>
2021-12-28 20:33:55 +0000

    - Deconst

--- libMotionCam/libMotionCam/include/motioncam/DngProcessorProgress.h
+++ libMotionCam/libMotionCam/include/motioncam/DngProcessorProgress.h
@@ -6,11 +6,11 @@
 namespace motioncam {
     class DngProcessorProgress {
     public:
-        virtual int onNeedFd(int threadNumber) const = 0;
-        virtual void onCompleted(int fd) const = 0;
-        virtual bool onProgressUpdate(int progress) const = 0;
-        virtual void onCompleted() const = 0;
-        virtual void onError(const std::string& error) const = 0;
+        virtual int onNeedFd(int frameNumber) = 0;
+        virtual void onCompleted(int fd) = 0;
+        virtual bool onProgressUpdate(int progress) = 0;
+        virtual void onCompleted() = 0;
+        virtual void onError(const std::string& error) = 0;
     };
 }
 
